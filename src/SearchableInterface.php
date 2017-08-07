298445464c2b2108603e5834190ae097ebdb98d3
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-08-07 10:41:40 +0300

    Initial commit

---
+++ src/SearchableInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Search
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Search;
+
+use Vainyl\Core\IdentifiableInterface;
+
+/**
+ * Interface SearchableInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface SearchableInterface extends IdentifiableInterface
+{
+}
